219c2f125febc0b126c84b930397bf1ded52c4fe
Si Tran <it.sitranvan@gmail.com>
2023-02-06 16:02:35 +0700

    commit

---
+++ hooks/useClickOutSide.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+
+function useClickOutSide(dom = "button") {
+    const [show, setShow] = useState(false);
+    const nodeRef = useRef(null);
+    useEffect(() => {
+        function handleClickOutSide(e) {
+            if (
+                nodeRef.current &&
+                !nodeRef.current.contains(e.target) &&
+                !e.target.matches(dom)
+            ) {
+                setShow(false);
+            }
+        }
+        document.addEventListener("click", handleClickOutSide);
+        return () => {
+            document.removeEventListener("click", handleClickOutSide);
+        };
+    }, []);
+    return {
+        show,
+        setShow,
+        nodeRef,
+    };
+}
+export default useClickOutSide
